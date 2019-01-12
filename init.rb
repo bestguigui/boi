2c303d0bc5a38c463940850e59036b498d5e72b3
bestguigui <guillaume.quillet@yahoo.fr>
2019-01-12 18:24:09 +0100

    seperating the Character class in seperate file

--- init.rb
+++ init.rb
@@ -1,32 +1,5 @@
 require 'gosu'
-
-class Character
-  def initialize(x = 0, y = 0)
-    @x, @y = x, y
-  end
-
-  def update
-    velocity = 2
-    # horizontal movement
-    if Gosu::button_down?(Gosu::KB_RIGHT)
-      @x += velocity
-    elsif Gosu::button_down?(Gosu::KB_LEFT)
-      @x -= velocity
-    end
-    # vertical movement
-    if Gosu::button_down?(Gosu::KB_DOWN)
-      @y += velocity
-    elsif Gosu::button_down?(Gosu::KB_UP)
-      @y -= velocity
-    end
-  end
-
-  def draw
-    size = 32
-    color = Gosu::Color::GREEN
-    Gosu::draw_rect(@x - size/2, @y - size/2, size, size, color)  
-  end
-end
+require_relative 'character.rb'
 
 class Window < Gosu::Window
   def initialize
