4ef14605411a76ba7dc861fe934dd500b2aed860
bestguigui <guillaume.quillet@yahoo.fr>
2019-01-12 18:18:03 +0100

    basic character class added

--- init.rb
+++ init.rb
@@ -1,9 +1,27 @@
 require 'gosu'
 
+class Character
+  def initialize(x = 0, y = 0)
+    @x, @y = x, y
+  end
+
+  def update
+
+  end
+
+  def draw
+    size = 32
+    color = Gosu::Color::GREEN
+    Gosu::draw_rect(@x - size/2, @y - size/2, size, size, color)  
+  end
+end
+
 class Window < Gosu::Window
   def initialize
     super(640, 480, false)
     self.caption = 'BoI clone'
+
+    @character = Character.new(self.width/2, self.height/2)
   end
 
   def needs_cursor?; true; end
@@ -14,11 +32,11 @@ class Window < Gosu::Window
   end
 
   def update
-
+  	@character.update
   end
 
   def draw
-
+    @character.draw
   end
 end
 
